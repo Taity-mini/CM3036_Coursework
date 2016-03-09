dd5351ac1fd75f9c1dcdc039c5a403f15415b960
Taity-mini <1504693@rgu.ac.uk>
2016-03-09 12:28:43 +0000

    initial commit

---
+++ CM3036_CW_1504693/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CM3036_CW_1504693
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// Coursework for CM3036 | Programming in C#
+    /// Author: Andrew Tait (1504693)
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
