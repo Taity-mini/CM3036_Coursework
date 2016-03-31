76b000f20577c1b66bdcca4c12b881ce8ebf1369
Taity-mini <1504693@rgu.ac.uk>
2016-03-31 18:26:52 +0100

    Redesigned database from sql and adding new student now works!

--- CM3036_CW_1504693/MainWindow.xaml.cs
+++ CM3036_CW_1504693/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace CM3036_CW_1504693
 {
@@ -33,12 +34,20 @@ namespace CM3036_CW_1504693
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
-            StudentsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("dBStudentViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
             // dBStudentViewSource.Source = [generic data source]
             context = new DBStudent();
-            //OnRefresh(sender, e);
+
+            StudentsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("studentViewSource")));
+            OnRefresh(sender, e);
+            // Load data by setting the CollectionViewSource.Source property:
+            // studentViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource dBStudentViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("dBStudentViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // dBStudentViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource studentViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("studentViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // studentViewSource.Source = [generic data source]
         }
 
         private void OnRefresh(object sender, RoutedEventArgs e)
@@ -47,7 +56,52 @@ namespace CM3036_CW_1504693
             // convert set of Appointment objects to list of Appointment objects
             Students = query.ToList();
             // connect list of Appointment entities to collection view
-            StudentsViewSource.Source =  Students;
+            StudentsViewSource.Source = Students;
+        }
+
+        //Adding a student's grades to the database
+        private void onAddStudent(object sender, RoutedEventArgs e)
+        {
+            //Field variables
+            string studentFirstName = firstName.Text;
+            string studentLastName = lastName.Text;
+            string studentMatriculation = matriculationNo.Text;
+            Trace.WriteLine(grade1.SelectedValue.ToString());
+            string studentGrade1 = grade1.SelectionBoxItem.ToString();
+            string studentGrade2 = grade2.SelectionBoxItem.ToString();
+            string studentGrade3 = grade3.SelectionBoxItem.ToString();
+            Trace.WriteLine(studentGrade1);
+            Trace.WriteLine(studentGrade2);
+            Trace.WriteLine(studentGrade3);
+           
+            string studentOverallGrade = "A"; //temp  for now..
+            Student students = new Student();
+            //Input validation will go here..
+           
+
+            //Calculate Overall Grade
+
+
+
+            //Add fields to database
+            students.firstName = studentFirstName;
+            students.lastName = studentLastName;
+            students.matricNum = studentMatriculation;
+            students.gradeOne = "A";
+            students.gradeTwo = "B";
+            students.gradeThree = "C";
+            students.gradeOverall = studentOverallGrade;
+            //students.Id = 1;
+
+            context.Students.Add(students);
+
+           
+
+            //Refresh the displayed list
+            OnRefresh(sender, e);
+            context.SaveChanges();
+
+            MessageBox.Show("Student Successfully Added!");
         }
 
 
