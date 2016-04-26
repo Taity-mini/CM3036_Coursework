200c8a659b1917c0ccce687d53ff3a43bb77c5b9
Taity-mini <1504693@rgu.ac.uk>
2016-04-26 11:39:57 +0100

    Added auto-fail condition if non-submission occurs

--- CM3036_CW_1504693/Functions.cs
+++ CM3036_CW_1504693/Functions.cs
@@ -96,142 +96,156 @@ namespace CM3036_CW_1504693
             int gradeD = 0;
             int gradeE = 0;
 
-            //Component one - Weighting x3
-            switch (grade1)
+            //First check for any non-submissions
+            bool nonSubmissions = false;
+
+            if (grade1 == " " || grade2 == " " || grade3 == " ")
             {
-                case "A":
-                    gradeA += 3;
-                    gradeB += 3;
-                    gradeC += 3;
-                    gradeD += 3;
-                    gradeE += 3;
-                    break;
-
-                case "B":
-                    gradeB += 3;
-                    gradeC += 3;
-                    gradeD += 3;
-                    gradeE += 3;
-                    break;
-
-                case "C":
-                    gradeC += 3;
-                    gradeD += 3;
-                    gradeE += 3;
-                    break;
-
-                case "D":
-                    gradeD += 3;
-                    gradeE += 3;
-                    break;
-
-                case "E":
-                    gradeE += 3;
-                    break;
-
-            }
-
-            //Component two - Weighting x5
-            switch (grade2)
-            {
-                case "A":
-                    gradeA += 5;
-                    gradeB += 5;
-                    gradeC += 5;
-                    gradeD += 5;
-                    gradeE += 5;
-                    break;
-
-                case "B":
-                    gradeB += 5;
-                    gradeC += 5;
-                    gradeD += 5;
-                    gradeE += 5;
-                    break;
-
-                case "C":
-                    gradeC += 5;
-                    gradeD += 5;
-                    gradeE += 5;
-                    break;
-
-                case "D":
-                    gradeD += 5;
-                    gradeE += 5;
-                    break;
-
-                case "E":
-                    gradeE += 5;
-                    break;
-            }
-
-            //Component three - Weighting x2
-            switch (grade3)
-            {
-                case "A":
-                    gradeA += 2;
-                    gradeB += 2;
-                    gradeC += 2;
-                    gradeD += 2;
-                    gradeE += 2;
-                    break;
-
-                case "B":
-                    gradeB += 2;
-                    gradeC += 2;
-                    gradeD += 2;
-                    gradeE += 2;
-                    break;
-
-                case "C":
-                    gradeC += 2;
-                    gradeD += 2;
-                    gradeE += 2;
-                    break;
-
-                case "D":
-                    gradeD += 2;
-                    gradeE += 2;
-                    break;
-
-                case "E":
-                    gradeE += 2;
-                    break;
-
-            }
-
-
-            //Calculate overall grade on each grade weighting
-            //Loop through Grade A to F
-
-            //Grade A
-            if (gradeA >= 5 && gradeB >= 7 && gradeC == 10)
-            {
-                overallGrade = "A";
-            }
-            //Grade B
-            else if (gradeB >= 5 && gradeC >= 7 && gradeD == 10)
-            {
-                overallGrade = "B";
-            }
-            //Grade C
-            else if (gradeC >= 5 && gradeD >= 7)
-            {
-                overallGrade = "C";
-            }
-            // Grade D
-            else if (gradeD >= 5 && gradeE >= 7)
-            {
-                overallGrade = "D";
-            }
-            //Grade E
-            else if (gradeE >= 7)
-            {
-                overallGrade = "E";
-            }
-            else
-            {
+                nonSubmissions = true;
                 overallGrade = "F";
+            }
+            else if (!nonSubmissions)
+            {
+
+                //Component one - Weighting x3
+                switch (grade1)
+                {
+                    case "A":
+                        gradeA += 3;
+                        gradeB += 3;
+                        gradeC += 3;
+                        gradeD += 3;
+                        gradeE += 3;
+                        break;
+
+                    case "B":
+                        gradeB += 3;
+                        gradeC += 3;
+                        gradeD += 3;
+                        gradeE += 3;
+                        break;
+
+                    case "C":
+                        gradeC += 3;
+                        gradeD += 3;
+                        gradeE += 3;
+                        break;
+
+                    case "D":
+                        gradeD += 3;
+                        gradeE += 3;
+                        break;
+
+                    case "E":
+                        gradeE += 3;
+                        break;
+
+                }
+
+                //Component two - Weighting x5
+                switch (grade2)
+                {
+                    case "A":
+                        gradeA += 5;
+                        gradeB += 5;
+                        gradeC += 5;
+                        gradeD += 5;
+                        gradeE += 5;
+                        break;
+
+                    case "B":
+                        gradeB += 5;
+                        gradeC += 5;
+                        gradeD += 5;
+                        gradeE += 5;
+                        break;
+
+                    case "C":
+                        gradeC += 5;
+                        gradeD += 5;
+                        gradeE += 5;
+                        break;
+
+                    case "D":
+                        gradeD += 5;
+                        gradeE += 5;
+                        break;
+
+                    case "E":
+                        gradeE += 5;
+                        break;
+                }
+
+                //Component three - Weighting x2
+                switch (grade3)
+                {
+                    case "A":
+                        gradeA += 2;
+                        gradeB += 2;
+                        gradeC += 2;
+                        gradeD += 2;
+                        gradeE += 2;
+                        break;
+
+                    case "B":
+                        gradeB += 2;
+                        gradeC += 2;
+                        gradeD += 2;
+                        gradeE += 2;
+                        break;
+
+                    case "C":
+                        gradeC += 2;
+                        gradeD += 2;
+                        gradeE += 2;
+                        break;
+
+                    case "D":
+                        gradeD += 2;
+                        gradeE += 2;
+                        break;
+
+                    case "E":
+                        gradeE += 2;
+                        break;
+
+                }
+
+
+                //Calculate overall grade on each grade weighting
+                //Loop through Grade A to F
+
+
+
+                //Grade A
+                if (gradeA >= 5 && gradeB >= 7 && gradeC == 10)
+                {
+                    overallGrade = "A";
+                }
+                //Grade B
+                else if (gradeB >= 5 && gradeC >= 7 && gradeD == 10)
+                {
+                    overallGrade = "B";
+                }
+                //Grade C
+                else if (gradeC >= 5 && gradeD >= 7)
+                {
+                    overallGrade = "C";
+                }
+                // Grade D
+                else if (gradeD >= 5 && gradeE >= 7)
+                {
+                    overallGrade = "D";
+                }
+                //Grade E
+                else if (gradeE >= 7)
+                {
+                    overallGrade = "E";
+                }
+                else
+                {
+                    overallGrade = "F";
+                }
             }
             return overallGrade;
         }
